9ac4c536ba79c9fb3d65d5b0b7fe89b0761a09f8
MRensen <markrensen@hotmail.com>
2018-03-13 12:06:55 +0100

    removed commented out and redundant code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,5 @@
 package com.company;
-
-
 import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,27 +9,6 @@ public class Main {
         //        Run your program with the following directed graph to find the shortest path from vertex   a   to vertex   e.
         //                Show the output of your program.
 
-
-        //        Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
-        //
-        //        Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all other nodes.
-        // Set the initial node as current.
-        //
-        //        For the current node, consider all of its unvisited neighbors and calculate their tentative distances through the current node.
-        // Compare the newly calculated tentative distance to the current assigned value and assign the smaller one.
-        // For example, if the current node A is marked with a distance of 6, and the edge connecting it with a neighbor B has length 2,
-        // then the distance to B through A will be 6 + 2 = 8. If B was previously marked with a distance greater than 8 then change it to 8.
-        // Otherwise, keep the current value.
-        //
-        //                When we are done considering all of the neighbors of the current node, mark the current node as visited and remove it from
-        // the unvisited set. A visited node will never be checked again.
-        //        If the destination node has been marked visited (when planning a route between two specific nodes) or if the smallest tentative
-        // distance among the nodes in the unvisited set is infinity (when planning a complete traversal; occurs when there is no connection between
-        // the initial node and remaining unvisited nodes), then stop. The algorithm has finished.
-        //        Otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new "current node", and go
-        // back to step 3.
-
-//        List<Vertex> v = new LinkedList();
         Map<String, Integer> v = new HashMap<String, Integer>();
         List<Edge> e = new LinkedList();
 
@@ -42,13 +18,6 @@ public class Main {
         v.put("d", -1);
         v.put("e", -1);
         v.put("f", -1);
-
-//        v.add(new Vertex(0, "a"));
-//        v.add(new Vertex(-1, "b"));
-//        v.add(new Vertex(-1, "c"));
-//        v.add(new Vertex(-1, "d"));
-//        v.add(new Vertex(-1, "e"));
-//        v.add(new Vertex(-1, "f"));
 
         e.add(new Edge("a", "b", 7));
         e.add(new Edge("a", "c", 9));
@@ -80,12 +49,8 @@ public class Main {
                     if (o.getStart().equals(entry.getKey())) {
                         if (v.get(o.getEnd()) < 0) {
                             v.put(o.getEnd(), entry.getValue() + o.getValue());
-                        } else if (/*iValue+o.value<=oEnd.value*/ (entry.getValue() + o.getValue()) < v.get(o.getEnd())) {
-                            // replace oEndValue in vMap
+                        } else if ((entry.getValue() + o.getValue()) < v.get(o.getEnd())) {
                             v.put(o.getEnd(), entry.getValue() + o.getValue());
-//                        } else {
-//                            System.out.println("how'd i get here");
-//                            System.out.println(entry.getKey()+o.getStart()+o.getEnd() +"/n" + entry.getValue() + " "+ o.getValue());
                         }
                     }
 
@@ -98,36 +63,6 @@ public class Main {
         }
     }
 }
-//class Vertex {
-//    public Vertex(int value, String name) {
-//        this.value = value;
-//        this.name = name;
-//    }
-//
-//    int value;
-//    String name;
-//    boolean visited = false;
-//
-//    public boolean isVisited() {
-//        return visited;
-//    }
-//
-//    public void setVisited(boolean visited) {
-//        this.visited = visited;
-//    }
-//
-//    public int getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(int value) {
-//        this.value = value;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//}
 
 class Edge {
     String end;
@@ -152,7 +87,4 @@ class Edge {
         return value;
     }
 
-    public void setValue(int value) {
-        this.value = value;
-    }
 }
